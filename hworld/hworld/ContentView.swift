4ac11acbdc8e0227e498c2a7aba43e54c7df34eb
Martin Sebastian Bulkowski <lp1357111317@icloud.com>
2019-10-27 19:31:32 +0100

    sketch
    empty ios project

---
+++ hworld/hworld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hworld
+//
+//  Created by Martin Bulkowski on 27/10/2019.
+//  Copyright © 2019 Martin Sebastian Bulkowski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Yo motherfucker haha lol jkjk don't be mad, pussy")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hworld/hworldTests/hworldTests.swift
@@ -0,0 +1,34 @@
+//
+//  hworldTests.swift
+//  hworldTests
+//
+//  Created by Martin Bulkowski on 27/10/2019.
+//  Copyright © 2019 Martin Sebastian Bulkowski. All rights reserved.
+//
+
+import XCTest
+@testable import hworld
+
+class hworldTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
